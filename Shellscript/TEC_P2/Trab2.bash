9d236e51dbe4d04fcf3d1e54cf2337b0b8d7b9c0
KibaNaoParsa <elyasnog2@hotmail.com>
2019-06-07 08:19:58 -0300

    Trabalho 2 (07/06/2019)

--- Shellscript/TEC_P2/Trab2.bash
+++ Shellscript/TEC_P2/Trab2.bash
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-arquivo="cefetmg_pavilhao_1.kml"
+echo "Insira o nome (ou endereço) do arquivo a ser analisado: "
+read arquivo
 
-nome=$(cat $arquivo | cut -f2 -s | head -n1)
-echo $nome
+nome=$(cat $arquivo | cut -f2 -s | head -n1 | cut -c7- | cut -d"<" -f1)
+nome=${nome::-4}
+echo "Nome do prédio: $nome"
 
-coordenadas=$(cat $arquivo | cut -f7 -s)
-echo $coordenadas
+coordenadas=$(cat $arquivo | cut -f7 -s | tr " " "\n")
+numero=$(cat $arquivo | cut -f7 -s | tr " " "\n" | wc -w)
+numero=$(($numero-1))
+
+coordx=0
+coordy=0
+
+for i in $(seq 1 $numero)
+do
+	valor=$i"p"
+	coordp=$(cat $arquivo | cut -f7 -s | tr " " "\n" | cut -d"," -f1 -s | sed -n $valor)
+	coordx=$(bc <<< "$coordx + $coordp")
+
+	coordp=$(cat $arquivo | cut -f7 -s | tr " " "\n" | cut -d"," -f2 -s | sed -n $valor)
+	coordy=$(bc <<< "$coordy + $coordp")
+
+done
+
+coordx=$(echo "$coordx / $numero" | bc -l)
+coordy=$(echo "$coordy / $numero" | bc -l)
+echo "Coordenadas do centro do prédio:"
+echo $coordx
+echo $coordy
